5fc49c5dbe8a6066555d665b341d77171a5db979
doyun <90900630+Doyun-coding@users.noreply.github.com>
2025-04-10 09:33:45 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/nhnmartcustomerservice/NhnMartCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nhnmartcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhnMartCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhnMartCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/nhnmartcustomerservice/NhnMartCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nhnmartcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhnMartCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
